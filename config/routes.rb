7160dd2df24743728bcf77253ef22add8c030b45
Haris Milonas <haris-mil_97@hotmail.com>
2022-09-28 11:05:58 +0300

    Add about Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
  root 'pages#home' #To pages einai to arxeio pou tha to steilw kai to #home einai to action(methodos) pou tha ektelesei
- 
+ get 'about', to: 'pages#about'
 end
