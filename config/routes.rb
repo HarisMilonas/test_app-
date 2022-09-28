2300ce271f85170c45aee94854e34aa71fab8e10
Haris Milonas <haris-mil_97@hotmail.com>
2022-09-28 13:04:51 +0300

    Scaffold and CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
  root 'pages#home' #To pages einai to arxeio pou tha to steilw kai to #home einai to action(methodos) pou tha ektelesei
  get 'about', to: 'pages#about'
 end
